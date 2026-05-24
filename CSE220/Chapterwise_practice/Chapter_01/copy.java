1c67bbbd82f06a081aff549dc3549e541d1a54f2
estiuk <mdestiukuzzaman@gmail.com>
2026-05-23 17:28:25 -0700

    Copy

---
+++ CSE220/Chapterwise_practice/Chapter_01/copy.java
@@ -0,0 +1,12 @@
+package CSE220.Chapterwise_practice.Chapter_01;
+
+public class copy {
+    public static void main(String[] args) {
+        int [] arr = {10, 20, 30, 40, 50};
+        int [] newArr = new int[5];
+
+        for(int i =0;i<arr.length;i++){
+            newArr[i]=arr[i];
+        }
+    }
+}
